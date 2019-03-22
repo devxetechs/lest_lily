5487318e32e2bbbe927b75fb6da8b18c9d263e5c
devxetechs <44792748+devxetechs@users.noreply.github.com>
2019-03-22 14:21:46 -0500

    [ADD] app fleet_operations

---
+++ fleet_operations/wizard/continue_pending_repair.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# See LICENSE file for full copyright and licensing details.
+
+from odoo import models
+
+
+class ContinuePendingRepair(models.TransientModel):
+
+    _name = 'continue.pending.repair'
+    _description = 'Vehicle Continue Pending Repair'
